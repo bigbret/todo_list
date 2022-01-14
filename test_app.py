cac46f9bdb5500771216c1b16749f4de7764cf1a
Domnika Popov <domnikapopov@Domnikas-MacBook-Pro.local>
2022-01-14 09:46:34 -0500

    first test

---
+++ test_app.py
@@ -0,0 +1,10 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
+def test_index(client):
+    resp = client.get('/')
+    assert resp.status_code == 200
